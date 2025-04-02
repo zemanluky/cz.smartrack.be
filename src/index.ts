319bca8817cd7ff88ff3ed6fc692d6679b275e37
zemanluky <zemanluky@gmail.com>
2025-04-02 19:43:15 +0200

    :sparkles: Integrate better auth config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import Elysia from "elysia";
+import {authController} from "./auth";
+import swagger from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+    .use(swagger())
+    .use(authController)
+    .listen(3000)
+;
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
