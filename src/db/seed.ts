db0f692dbd1efe7613d7d5a45d68091b4c0cd098
zemanluky <zemanluky@gmail.com>
2025-04-23 02:19:09 +0200

    :bug: Disable drizzle seeders as they are ignoring the id sequences

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -5,63 +5,68 @@ import { product } from "./schema/product";
 import {organization} from "./schema/organization";
 
 async function main() {
-    const password = (await Bun.password.hash('123456Ab$')).trim();
-
     await reset(db, { organization: organization, user: user, product: product });
 
-    await seed(db, { organization: organization, user: user, product: product }).refine((funcs) => ({
-        organization: {
-            count: 30,
-            columns: {
-                name: funcs.companyName({ isUnique: true }),
-                active: funcs.boolean()
-            },
-            with: {
-                user: 3,
-                product: 80
-            }
-        },
-        product: {
-            columns: {
-                name: funcs.string(),
-                price: funcs.number({ precision: 100, minValue: 1 }),
-                deleted_at: funcs.weightedRandom([
-                    {
-                        weight: 0.15,
-                        value: funcs.default({ defaultValue: null }),
-                    },
-                    {
-                        weight: 0.85,
-                        value: funcs.timestamp()
-                    }
-                ])
-            }
-        },
-        user: {
-            columns: {
-                organization_id: funcs.default({ defaultValue: null }),
-                role: funcs.valuesFromArray({ values: ['org_admin', 'org_user'] }),
-                email: funcs.email(),
-                password_hash: funcs.default({ defaultValue: password }),
-                name: funcs.fullName()
-            }
-        },
-    })).then(() => seed(db, { user: user }).refine((funcs) => ({
-            user: {
-                count: 3,
-                columns: {
-                    organization_id: funcs.default({ defaultValue: null }),
-                    role: funcs.default({ defaultValue: 'sys_admin' }),
-                    email: funcs.valuesFromArray({
-                        values: ['admin@smartrack.cz', 'admin2@smartrack.cz', 'admin3@smartrack.cz'],
-                        isUnique: true
-                    }),
-                    password_hash: funcs.default({ defaultValue: password }),
-                    name: funcs.fullName()
-                }
-            },
-        })
-    ));
+    await db.insert(user).values({
+        role: 'sys_admin',
+        name: 'Admin Smart Rack',
+        email: 'admin@smartrack.cz',
+        password_hash: await Bun.password.hash('123456Ab$')
+    });
+
+    // await seed(db, { organization: organization, user: user, product: product }).refine((funcs) => ({
+    //     organization: {
+    //         count: 30,
+    //         columns: {
+    //             name: funcs.companyName({ isUnique: true }),
+    //             active: funcs.boolean()
+    //         },
+    //         with: {
+    //             user: 3,
+    //             product: 80
+    //         }
+    //     },
+    //     product: {
+    //         columns: {
+    //             name: funcs.string(),
+    //             price: funcs.number({ precision: 100, minValue: 1 }),
+    //             deleted_at: funcs.weightedRandom([
+    //                 {
+    //                     weight: 0.15,
+    //                     value: funcs.default({ defaultValue: null }),
+    //                 },
+    //                 {
+    //                     weight: 0.85,
+    //                     value: funcs.timestamp()
+    //                 }
+    //             ])
+    //         }
+    //     },
+    //     user: {
+    //         columns: {
+    //             organization_id: funcs.default({ defaultValue: null }),
+    //             role: funcs.valuesFromArray({ values: ['org_admin', 'org_user'] }),
+    //             email: funcs.email(),
+    //             password_hash: funcs.default({ defaultValue: password }),
+    //             name: funcs.fullName()
+    //         }
+    //     },
+    // })).then(() => seed(db, { user: user }).refine((funcs) => ({
+    //         user: {
+    //             count: 3,
+    //             columns: {
+    //                 organization_id: funcs.default({ defaultValue: null }),
+    //                 role: funcs.default({ defaultValue: 'sys_admin' }),
+    //                 email: funcs.valuesFromArray({
+    //                     values: ['admin@smartrack.cz', 'admin2@smartrack.cz', 'admin3@smartrack.cz'],
+    //                     isUnique: true
+    //                 }),
+    //                 password_hash: funcs.default({ defaultValue: password }),
+    //                 name: funcs.fullName()
+    //             }
+    //         },
+    //     })
+    // ));
 }
 
 try {
